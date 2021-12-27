0a641273f44fcfd2a881b210492072e1eccbefb1
becket <becket.bowes@gmail.com>
2021-12-27 16:05:48 -0500

    added crypto randomness to previous version

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-// first functioning attempt to create password with Math.random
+
 function generatePassword() {
 //crypto.getRandomValues(array1: ArrayBuffer)
 var password = ""
-var array2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*(){}[]\|";
+var array1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*(){}[]\|";
+let uuid = self.crypto.randomUUID();
 for (i = 1; i <=11; i++) {
-  var char = Math.floor(Math.random() * array2.length + 1);
-  password += array2.charAt(char);
+  var char = Math.floor(Math.random(uuid) * array1.length + 1);
+  password += array1.charAt(char);
 };
 return password;
-}
+};
+// var array2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*(){}[]\|";
+// for (i = 1; i <=11; i++) {
+//   var char = Math.floor(Math.random() * array2.length + 1);
+//   password += array2.charAt(char);
+// };
+// return password;
+// }
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
