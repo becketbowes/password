3b0917aba90a9eaddd63971914a8172855d5a515
becket <becket.bowes@gmail.com>
2021-12-27 23:43:45 -0500

    pretty pretty pretty functional

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// some global variables
+//just some global variables for everyone to play with
 var array1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var array2 = "abcdefghijklmnopqrstuvwxyz";
 var array3 = "0123456789";
@@ -9,7 +9,7 @@ var arrayAlpha = (array1 + array2 + array3 + array4);
 //Determine password length
 //8-128 charcters
 var codeLength = function() {
-  var howLong = "88";
+  var howLong = "";
   var howLong = window.prompt("How many characters would you like the password to be? Please enter a number between 8 and 128");
   howLong = parseInt(howLong);
 if (howLong > 128 || howLong < 8) {
@@ -48,11 +48,11 @@ var codeType = function() {
   var whatKind = window.confirm("Finally, would you like any special characters with your code?");
   if (whatKind) {
     this.special = array4;
-    doIt();
     localStorage.setItem("string", (this.uppercase + this.lowercase + this.numbers + this.special));
+    doIt();
   } else {
-    doIt();
     localStorage.setItem("string", (this.uppercase + this.lowercase + this.numbers + this.special));
+    doIt();
   }
   return whatKind;
 };
@@ -60,10 +60,14 @@ var codeType = function() {
 //question 3
 //execute algorythm
 var doIt = function() {
-  window.confirm("Shall we?");
+  var bananaArray = localStorage.getItem("string");
+  var bananaLength = localStorage.getItem("size");
+  window.confirm("So, you want a rando " + bananaLength + " length string of this type of stuff: " + bananaArray + " Yeah? Shall we?");
   if (doIt) {
     writePassword();
-  } 
+  } else {
+    codeLength();
+  }
 };
 
 //putting cryptoUUID in addition to the Math.random with full function for preplaned length and charset
@@ -76,8 +80,8 @@ function generatePassword() {
   var password = ""
   let uuid = self.crypto.randomUUID();
   for (i = 1; i <=bananaLength; i++) {
-    var char = Math.floor(Math.random(uuid) * arrayAlpha.length + 1);
-    password += arrayAlpha.charAt(char);
+    var char = Math.floor(Math.random(uuid) * bananaArray.length + 1);
+    password += bananaArray.charAt(char);
   };
   return password;
   };
